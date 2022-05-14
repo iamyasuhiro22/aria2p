11a75c00baa9b6fd5e2e16570587b4d4b57bf008
iamyasuhiro22 <105473710+iamyasuhiro22@users.noreply.github.com>
2022-05-14 20:58:07 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="johndoe-aria2p",
+    version="0.11.2",
+)
